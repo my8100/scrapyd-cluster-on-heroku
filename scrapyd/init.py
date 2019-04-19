78504b86b0a0f5b6a156dcfb51221f652bd2f9ee
my8100 <my8100@gmail.com>
2019-04-19 12:54:03 +0800

    Update init.py

--- scrapyd/init.py
+++ scrapyd/init.py
@@ -4,17 +4,21 @@ import os
 from subprocess import Popen
 
 
-PORT = int(os.environ.get('PORT', 6800))
+# https://devcenter.heroku.com/articles/runtime-principles#web-servers
+# The port to bind to is assigned by Heroku as the PORT environment variable.
+PORT = os.environ['PORT']
 with io.open("scrapyd.conf", 'r+', encoding='utf-8') as f:
     f.read()
     f.write(u'\nhttp_port = %s\n' % PORT)
 
 
+# Launch LogParser as a subprocess
 logs_dir = '/app/logs'
 if not os.path.exists(logs_dir):
     os.mkdir(logs_dir)
+
 args = ['logparser', '-dir', logs_dir]
-args.extend(['--scrapyd_server', os.environ.get('SCRAPYD_SERVER', '127.0.0.1:6800')])
+args.extend(['--scrapyd_server', '127.0.0.1:%s' % PORT])
 args.extend(['--sleep', os.environ.get('PARSE_ROUND_INTERVAL', '10')])
 if os.environ.get('ENABLE_TELNET', 'True') == 'False':
     args.extend(['--disable_telnet'])
